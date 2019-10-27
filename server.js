39de644ad3f534f9722ee2da467bf811a7cc0f96
Tejas Srivastava <tjss@seas.upenn.edu>
2019-10-26 20:46:41 -0400

    created users table, inserting users on registration, opening profile page

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 
 const webapp = express();
+
 const bodyParser = require('body-parser');
 const db = require('./database.js');
+// alert("db");
 
 webapp.use(express.static('html'))
 
@@ -29,6 +31,61 @@ webapp.get('/posts/:id', (req, res) => {
     res.json({
       message: 'success',
       data: rows,
+    });
+  });
+});
+
+
+webapp.get('/users', (req, res) => {
+  console.log('READ all users');
+  const sql = 'select * from users';
+  const params = [];
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      res.status(404).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows,
+    });
+  });
+});
+
+webapp.post("/user_register", (req, res) => {
+  // alert("hello");
+  console.log('CREATING a new user');
+  if (!req.body.username || !req.body.email || !req.body.fname || !req.body.lname || !req.body.password) {
+    res.status(400).json({ error: 'missing username or email or firstName or lastName or password' });
+    return;
+  }
+  // create student object
+  const newUser = {
+    // user_id: req.body.user_id,
+    username: req.body.username,
+    fname: req.body.fname,
+    lname: req.body.lname,
+    email: req.body.email,
+    password: req.body.password,
+    // followers: req.body.followers,
+    // following: req.body.following,
+    // postcount: req.body.postcount
+  };
+  console.log(newUser);
+  //insert newStudent
+  const insert = 'INSERT INTO users (username, fname, lname, email, password) VALUES (?,?,?,?,?)';
+  // const sql = 'INSERT INTO users (username, fname, lname, email, password) VALUES (?,?,?,?,?)';
+  const values = [newUser.username, newUser.fname, newUser.lname, newUser.email, newUser.password];
+  db.run(insert, values, function (err, result) {
+    if (err) {
+      console.log("errrrrrrr");
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      user: newUser,
+      id: this.lastID,
     });
   });
 });
